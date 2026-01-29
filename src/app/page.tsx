0680c742a29682f41c41b5c75e26a985cf5f6e49
Irfan Irawan Sukirman <irfani.sukirman@gmail.com>
2026-01-29 02:56:03 +0000

    wavenya kurang pas terutama bagian kiri, jangan ada patah-patah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-8 md:gap-12">
       <section className="relative bg-primary w-full text-primary-foreground">
-        <div className="relative z-10 flex h-[60vh] min-h-[500px] flex-col items-center justify-center text-center p-4">
+        <div className="relative z-10 flex h-[80vh] min-h-[480px] flex-col items-center justify-center text-center p-4">
           <h1 className="text-5xl md:text-7xl font-extrabold tracking-tight">
             <span className="block">Berbagi Pengetahuan dan</span>
             <span className="block text-accent">Membangun Kompetensi</span>
@@ -35,7 +35,7 @@ export default function Home() {
         </div>
         <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none z-0">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="relative block w-[calc(100%+1.3px)] h-[120px]">
-                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.39-16.72-168.19-17.73-250.45-.39-52.13,10.8-108.28,22.73-162.63,30.45-29.7,4.2-59.57,4.6-88.66,2.3-29.09-2.3-58.1-7.2-86.5-14.7-28.4-7.5-56.3-17.4-82.9-29.5C62.2,5.4,31.1,0,0,0v120h1200V95.8c-67.81,23.12-144.29,15.51-214.34-3Z" className="fill-background"></path>
+                <path d="M0,100 Q600,60 1200,100 V120 H0 Z" className="fill-background"></path>
             </svg>
         </div>
       </section>
